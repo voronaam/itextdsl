373f4ff76e8e3cf46457e4ae9f2daaf57e5a3cd7
Aleksey Vorona <voronaam@gmail.com>
2013-10-20 00:52:25 -0700

    Further defining desired DSL

--- src/main/scala/DslTest.scala
+++ src/main/scala/DslTest.scala
@@ -1,11 +1,30 @@
 import ca.vorona.iext.dsl.PDF
 import java.util.Date
+import java.awt.Color
 
 object DslTest extends App {
 
-  new PDF { 
+  import com.lowagie.text.Font;
+
+  new PDF {
     file("/tmp/HelloWorld.pdf")
     paragraph(s"Hello World ${new Date()}")
+    
+    paragraph {
+      phrase {
+        chunk {
+          font (
+            family = Font.COURIER,
+            size = 10,
+            style = Font.BOLD,
+            color = new Color(0x92, 0x90, 0x83) // TODO: rgb"929083"
+          )
+          background(new Color(0xff, 0xe4, 0x00)) // TODO: rgb"ffe400"
+          "testing text element "
+        }
+        "This is initial text. "
+      }
+    }
     close()
   }
 }
--- src/main/scala/ca/vorona/iext/dsl/PDF.scala
+++ src/main/scala/ca/vorona/iext/dsl/PDF.scala
@@ -4,6 +4,9 @@ import com.lowagie.text.Document
 import com.lowagie.text.pdf.PdfWriter
 import com.lowagie.text.Paragraph
 import java.io.FileOutputStream
+import com.lowagie.text.Phrase
+import com.lowagie.text.Chunk
+import java.awt.Color
 
 abstract class PDF extends Document {
 
@@ -15,5 +18,25 @@ abstract class PDF extends Document {
   def paragraph(contents: String) {
     add(new Paragraph(contents))
   }
+
+  def paragraph(contents: Phrase) {
+    add(new Paragraph(contents))
+  }
+
+  def phrase(body: String) = {
+    new Phrase(body)
+  }
   
+  def chunk(body: String) = {
+    new Chunk
+  }
+
+  def font(size: Int = 0, family: Int = 0, style: Int = 0, color: Color = null) = {
+    
+  }
+  
+  def background(color:Color) = {
+    
+  }
+
 }
